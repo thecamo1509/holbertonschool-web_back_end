0bdd06c739ba8368477902102dbcf73348add44d
Camilo Morales <camilo@rootedlending.com>
2023-03-29 23:24:38 -0500

    Updating

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -5,5 +5,5 @@ export default function createInt8TypedArray(length, position, value) {
     throw new Error('Position outside range');
   }
   view[position] = value;
-  return buffer;
+  return new DataView(buffer);
 }
