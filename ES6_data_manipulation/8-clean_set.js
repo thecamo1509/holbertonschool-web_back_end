7f773bf033473a57d7317be421798b07b7bd1230
Camilo Morales <camilo@rootedlending.com>
2023-03-29 23:53:04 -0500

    Updating

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,16 @@
+export default function cleanSet(set, startString) {
+  let fullString = '';
+  set.forEach((element) => {
+    if (startString.length > 0) {
+      if (element.startsWith(startString, 0)) {
+        const word = element.replace(startString, '');
+        if (fullString.length === 0) {
+          fullString += word;
+        } else {
+          fullString += `-${word}`;
+        }
+      }
+    }
+  });
+  return fullString;
+}
