e0892557c880671a9e790ce3440ff8b060d3f6c5
Camilo Morales <camilo@rootedlending.com>
2023-03-30 00:58:13 -0500

    Updating

--- ES6_data_manipulation/100-main.js
+++ ES6_data_manipulation/100-main.js
@@ -1,15 +1,12 @@
 import { queryAPI, weakMap } from "./100-weak.js";
 
 const endpoint = { protocol: 'http', name: 'getUsers' };
-weakMap.get(endpoint);
-
-queryAPI(endpoint);
-console.log(weakMap.get(endpoint));
-
-queryAPI(endpoint);
-console.log(weakMap.get(endpoint));
-
-queryAPI(endpoint);
-queryAPI(endpoint);
-queryAPI(endpoint);
-queryAPI(endpoint);
+    console.log("asdasd",weakMap.has(endpoint))
+    queryAPI(endpoint);
+    console.log("asdasd",weakMap.has(endpoint))
+    console.log("asdasd",weakMap.get(endpoint))
+    queryAPI(endpoint);
+    console.log("asdasd",weakMap.get(endpoint))
+    queryAPI(endpoint);
+    queryAPI(endpoint);
+    console.log("asdasd",weakMap.get(endpoint))
--- ES6_data_manipulation/100-weak.js
+++ ES6_data_manipulation/100-weak.js
@@ -3,7 +3,7 @@ export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   const count = weakMap.get(endpoint) || 0;
 
-  if (count >= 5) {
+  if (count >= 4) {
     throw new Error('Endpoint load is high');
   }
 
