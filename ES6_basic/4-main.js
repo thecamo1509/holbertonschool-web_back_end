de317e744a026d83487caacfb30955d9fa010c7c
Camilo Morales <camilo@rootedlending.com>
2023-03-30 11:46:40 -0500

    Updating

--- ES6_basic/4-main.js
+++ ES6_basic/4-main.js
@@ -2,3 +2,4 @@ import returnHowManyArguments from './4-rest-parameter.js';
 
 console.log(returnHowManyArguments("one"));
 console.log(returnHowManyArguments("one", "two", 3, "4th"));
+console.log(returnHowManyArguments(10,20,30,4).length);
