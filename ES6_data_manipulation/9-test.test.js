dae1b5e565a6f1b569f778ce28337d0968f6da8c
Camilo Morales <camilo@rootedlending.com>
2023-03-30 00:35:59 -0500

    Updating

--- ES6_data_manipulation/9-test.test.js
+++
@@ -1,6 +0,0 @@
-import groceriesList from "./9-groceries_list.js";
-
-test("map has a key 'Apples' that maps to 10", () => {
-  const map = groceriesList();
-  expect(map.get('Apples')).toBe(10);
-});
