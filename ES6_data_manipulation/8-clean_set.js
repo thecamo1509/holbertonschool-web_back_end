0166eecb1a4658ca43a6ef298cb8f7518d5a1d8b
Camilo Morales <camilo@rootedlending.com>
2023-03-29 23:58:49 -0500

    Updating

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,4 @@
-export default function cleanSet(set, startString) {
+export default function cleanSet(set, startString = '') {
   let fullString = '';
   set.forEach((element) => {
     if (startString.length > 0) {
--- ES6_data_manipulation/8-main.js
+++ ES6_data_manipulation/8-main.js
@@ -2,3 +2,4 @@ import cleanSet from "./8-clean_set.js";
 
 console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
 console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
+console.log(cleanSet(new Set(['id-test', 'id-chicken', 'id-user', 'id-id-']),'id-'));
