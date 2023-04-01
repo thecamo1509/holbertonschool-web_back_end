eed64fea3410e4543839bc5f244fcf660aa8d694
Camilo Morales <camilo@rootedlending.com>
2023-03-31 19:07:39 -0500

    Updating promise

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,7 +1,7 @@
 export default function getFullResponseFromAPI(success) {
   const newPromise = new Promise((resolve, reject) => {
     if (success === true) {
-      resolve({ status: 200, body: 'sucess' });
+      resolve({ status: 200, body: 'success' });
     } else {
       reject(new Error('The fake API is not working currently'));
     }
---
+++ ES6_promise/1-test.test.js
@@ -0,0 +1,9 @@
+import getFullResponseFromAPI from './1-promise';
+
+test("getFullResponseFromAPI resolves correctly", () => {
+    const successResponse = getFullResponseFromAPI(true);
+    return expect(successResponse).resolves.toEqual({
+      status: 200,
+      body: "success"
+    });
+});
