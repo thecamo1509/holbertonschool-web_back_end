6823fd047b0ba53763c0629463b7f8ac638bc32f
Camilo Morales <camilo@rootedlending.com>
2023-04-01 10:24:04 -0500

    Updating

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,12 +1,17 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  uploadPhoto().then((response) => {
-    createUser().then((data) => {
-      console.log(`${response.body} ${data.firstName} ${data.lastName}`);
-    }).catch(() => {
-      console.error('Signup system offline');
+  Promise.all([uploadPhoto(), createUser()]).then((values) => {
+    let response = '';
+    values.map((value) => {
+      if (value.body) {
+        response = `${response + value.body} `;
+      } else {
+        response = `${response + value.firstName} ${value.lastName}`;
+      }
+      return response;
     });
+    console.log(response);
   }).catch(() => {
     console.error('Signup system offline');
   });
