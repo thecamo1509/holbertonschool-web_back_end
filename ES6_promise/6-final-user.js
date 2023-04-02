cfc8895829053b9eb40e95fbe8b958406147ad0c
Camilo Morales <camilo@rootedlending.com>
2023-04-01 22:52:19 -0500

    Updating

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -6,6 +6,6 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
 
   return Promise.allSettled(promises).then((results) => results.map((result) => ({
     status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
+    value: result.status === 'fulfilled' ? result.value : String(result.reason),
   })));
 }
