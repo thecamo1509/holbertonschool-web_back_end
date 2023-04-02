4b31a69be85e06c683c1712daccd82480b7c9541
Camilo Morales <camilo@rootedlending.com>
2023-04-02 00:56:40 -0500

    updating

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,8 +1,5 @@
 export default class Building {
   constructor(sqft) {
-    if (new.target !== Building) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
     this._sqft = sqft;
   }
 
